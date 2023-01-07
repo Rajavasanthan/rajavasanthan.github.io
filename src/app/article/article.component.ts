68cf8b466e9ae3aa56b56a696d235ea1cd2c05c2
rajavasanthan <rajavasanthan@gmail.com>
2023-01-07 15:15:00 +0530

    added docs

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -8,56 +8,41 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ArticleComponent {
   articleId:string = '';
-  activeArticle: any;
+  activeArticle: any = {
+    id: "",
+    title: "",
+    subTitle: "",
+    summary: "",
+    imageUrl: "",
+    bannerImageUrl: ""
+  };
   eventJson = [
     {
       id: "1",
+      slug : "two-days-workshop-in-pailagam",
       title: "First Event",
       subTitle: "First Event Sub Title",
       summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur id   aspernatur sint nulla autem ducimus impedit in, consequatur reiciendis  non.Optio error nesciunt pariatur deserunt magni, aliquam iure blanditiis delectus voluptate, quibusdam explicabo, maiores odio! Repellat officiis sequi recusandae laudantium quos modi, dolore impedit vero eum tempora id delectus est quae aut nisi, veritatis eius ab pariatur magni, ad laboriosam.Maxime consequuntur eius placeat voluptate tempore veritatis vitae, corrupti assumenda harum nam voluptatem ratione ab, dolore suscipit hic reprehenderit saepe",
       imageUrl: "https://dummyimage.com/350x300",
-      bannerImageUrl: "https://dummyimage.com/1200x300"
+      bannerImageUrl: "assets/images/1200x300.png"
     },
     {
       id: "2",
+      slug : "vaathiyar-ai-got-incubation-in-iit-madras",
       title: "Second Event",
       subTitle: "Second Event Sub Title",
       summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur id   aspernatur sint nulla autem ducimus impedit in, consequatur reiciendis  non.Optio error nesciunt pariatur deserunt magni, aliquam iure blanditiis delectus voluptate, quibusdam explicabo, maiores odio! Repellat officiis sequi recusandae laudantium quos modi, dolore impedit vero eum tempora id delectus est quae aut nisi, veritatis eius ab pariatur magni, ad laboriosam.Maxime consequuntur eius placeat voluptate tempore veritatis vitae, corrupti assumenda harum nam voluptatem ratione ab, dolore suscipit hic reprehenderit saepe",
       imageUrl: "https://dummyimage.com/350x300",
-      bannerImageUrl: "https://dummyimage.com/1200x300"
-    },
-    {
-      id: "3",
-      title: "Third Event",
-      subTitle: "Third Event Sub Title",
-      summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur id   aspernatur sint nulla autem ducimus impedit in, consequatur reiciendis  non.Optio error nesciunt pariatur deserunt magni, aliquam iure blanditiis delectus voluptate, quibusdam explicabo, maiores odio! Repellat officiis sequi recusandae laudantium quos modi, dolore impedit vero eum tempora id delectus est quae aut nisi, veritatis eius ab pariatur magni, ad laboriosam.Maxime consequuntur eius placeat voluptate tempore veritatis vitae, corrupti assumenda harum nam voluptatem ratione ab, dolore suscipit hic reprehenderit saepe",
-      imageUrl: "https://dummyimage.com/350x300",
-      bannerImageUrl: "https://dummyimage.com/1200x300"
-    },
-    {
-      id: "4",
-      title: "Fourth Event",
-      subTitle: "Fourth Event Sub Title",
-      summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur id   aspernatur sint nulla autem ducimus impedit in, consequatur reiciendis  non.Optio error nesciunt pariatur deserunt magni, aliquam iure blanditiis delectus voluptate, quibusdam explicabo, maiores odio! Repellat officiis sequi recusandae laudantium quos modi, dolore impedit vero eum tempora id delectus est quae aut nisi, veritatis eius ab pariatur magni, ad laboriosam.Maxime consequuntur eius placeat voluptate tempore veritatis vitae, corrupti assumenda harum nam voluptatem ratione ab, dolore suscipit hic reprehenderit saepe",
-      imageUrl: "https://dummyimage.com/350x300",
-      bannerImageUrl: "https://dummyimage.com/1200x300"
-    },
-    {
-      id: "5",
-      title: "Fifth Event",
-      subTitle: "Fifth Event Sub Title",
-      summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur id   aspernatur sint nulla autem ducimus impedit in, consequatur reiciendis  non.Optio error nesciunt pariatur deserunt magni, aliquam iure blanditiis delectus voluptate, quibusdam explicabo, maiores odio! Repellat officiis sequi recusandae laudantium quos modi, dolore impedit vero eum tempora id delectus est quae aut nisi, veritatis eius ab pariatur magni, ad laboriosam.Maxime consequuntur eius placeat voluptate tempore veritatis vitae, corrupti assumenda harum nam voluptatem ratione ab, dolore suscipit hic reprehenderit saepe",
-      imageUrl: "https://dummyimage.com/350x300",
-      bannerImageUrl: "https://dummyimage.com/1200x300"
+      bannerImageUrl: "/assets/images/1200x300.png"
     }
   ]
   constructor(private activeRouter: ActivatedRoute) {
     console.log('ArticleComponent constructor');
     this.articleId = this.activeRouter.snapshot.params['id'];
-    this.eventJson.filter((res: any) => {
-      if(res.id === this.articleId) {
+    console.log(this.articleId);
+    this.eventJson.find((res: any) => {
+      if(res.slug === this.articleId) {
         this.activeArticle = res
-        console.log(res)
       }
     })
   }
