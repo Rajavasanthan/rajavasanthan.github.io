a20b7efa21c09aeca0da6f85b4555cf112a36e1b
rajavasanthan <rajavasanthan@gmail.com>
2023-01-08 15:17:18 +0530

    meta added

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,26 +1,44 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from '../article.service';
 import { Article } from '../Model/model';
 
-
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent {
-  articleSlug!:string;
-  article!:Article
-  
-  constructor(private activeRouter: ActivatedRoute,private articleService:ArticleService) {
+  articleSlug!: string;
+  article!: Article;
+
+  constructor(
+    private activeRouter: ActivatedRoute,
+    private articleService: ArticleService,
+    private title: Title,
+    private meta: Meta
+  ) {
     this.articleSlug = this.activeRouter.snapshot.params['id'];
-    
+
     this.articleService.eventJson.find((article: any) => {
-      if(article.slug === this.articleSlug) {
-        console.log(article)
-        this.article = article
+      if (article.slug === this.articleSlug) {
+        console.log(article);
+        this.article = article;
+        this.title.setTitle(article.title);
+        this.meta.addTag({
+          content: article.title,
+          name: 'og:title',
+        });
+        this.meta.addTag({
+          content: `${window.location.hostname}/${article.imageUrl}`,
+          name: 'og:image',
+        });
+        this.meta.addTag({
+          content: `${article.summary}`,
+          name: 'og:description',
+        });
       }
-    })
+    });
   }
 }
