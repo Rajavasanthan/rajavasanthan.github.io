e855e03d8cd97a788895a4ed02fbae0d4f8fbc91
rajavasanthan <rajavasanthan@gmail.com>
2023-04-09 12:11:48 +0530

    added google dev article

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,32 +1,48 @@
 export const articles = [
-    {
-      slug : "my-experience-with-stable-diffusion-and-how-it-transformed-my-images",
-      title: "My Experience with Stable Diffusion and How it Transformed My Images",
-      subTitle: "Jan 20 2023",
-      summary: `In this article, I share my experience using a stable diffusion AI model to create a new and different look for myself. I explain how the model works and how easy it was for me to use, thanks to the guidance provided by my brother Dhuriyothanan. I also highlight the benefits of using stable diffusion, such as its ability to create natural and flattering poses, spot-on lighting, and the ability to experiment with different looks and styles. Overall, this article is a personal account of how I used the stable diffusion AI model to enhance my images and how it can be a powerful tool for anyone who wants to experiment with their own photos.`,
-      imageUrl: "assets/images/my-experience-with-stable-diffusion-and-how-it-transformed-my-images.png",
-      bannerImageUrl: "assets/images/1200x300.png",
-      markDownFile : "assets/markdowns/my-experience-with-stable-diffusion-and-how-it-transformed-my-images.md",
-      date: new Date("Jan 20 2023")
-    },
-    {
-      slug : "old-it-dood-learning-has-no-limits",
-      title: "Old IT Dood (Learning has no limits)",
-      subTitle: "Jun 14 2022",
-      summary: `I met an older software developer named Gopalakrishnan who had been learning new technologies from my videos. He shared with me his background as a successful developer in the 90s but struggled to adapt to new technologies. I was impressed by his determination to improve his skills and shared my knowledge on modern software development practices and SaaS platforms. It was a reminder that it's never too late to learn and grow in the tech industry. I will continue to create and share knowledge through my videos to help others like Gopalakrishnan.`,
-      imageUrl: "assets/images/old-it-dood-learning-has-no-limits.jpeg",
-      bannerImageUrl: "assets/images/1200x300.png",
-      markDownFile : "assets/markdowns/old-it-dood-learning-has-no-limits.md",
-      date: new Date("Jun 14 2022")
-    },
-    {
-      slug : "two-days-workshop-in-payilagam",
-      title: "Two Days Workshop in Payilagam",
-      subTitle: "Nov 23 2023",
-      summary: `I have recently led a successful 2-day seminar on HTML and CSS at Paiylagam Institute, where the students were attentive and interactive. The instructor effectively imparted their knowledge of web development to the students, who actively participated in hands-on exercises and a real-world project. At the end of each day, the instructor held a fun review game to reinforce the material learned, creating a balanced and enjoyable learning experience. The seminar was a resounding success and the instructor looks forward to leading more seminars in the future.`,
-      imageUrl: "assets/images/pailagam.jpeg",
-      bannerImageUrl: "assets/images/1200x300.png",
-      markDownFile : "assets/markdowns/two-days-workshop-in-payilagam.md",
-      date: new Date("Jun 17 2022")
-    }
-  ]
+  {
+    slug: 'google-developers-student-club-organizes-tech-event-vit-chennai-campus',
+    title:
+      'Google Developers Student Club Organizes Tech Event in VIT Chennai Campus',
+    subTitle: 'Mar 8th 2023',
+    summary: `On April 8th, 2023, the Google Developers Student Club at VIT Chennai Campus organised a one-day full tech event, which brought together enthusiastic students from various colleges in the area. The event featured several talks on different aspects of technology and ended with a final panel discussion, where panel members answered questions from the students.`,
+    imageUrl:
+      'assets/images/google-developers-student-club-organizes-tech-event-vit-chennai-campus.jpg',
+    bannerImageUrl: 'assets/images/1200x300.png',
+    markDownFile:
+      'assets/markdowns/google-developers-student-club-organizes-tech-event-vit-chennai-campus.md',
+    date: new Date('Mar 8 2023'),
+  },
+  {
+    slug: 'my-experience-with-stable-diffusion-and-how-it-transformed-my-images',
+    title:
+      'My Experience with Stable Diffusion and How it Transformed My Images',
+    subTitle: 'Jan 20 2023',
+    summary: `In this article, I share my experience using a stable diffusion AI model to create a new and different look for myself. I explain how the model works and how easy it was for me to use, thanks to the guidance provided by my brother Dhuriyothanan. I also highlight the benefits of using stable diffusion, such as its ability to create natural and flattering poses, spot-on lighting, and the ability to experiment with different looks and styles. Overall, this article is a personal account of how I used the stable diffusion AI model to enhance my images and how it can be a powerful tool for anyone who wants to experiment with their own photos.`,
+    imageUrl:
+      'assets/images/my-experience-with-stable-diffusion-and-how-it-transformed-my-images.png',
+    bannerImageUrl: 'assets/images/1200x300.png',
+    markDownFile:
+      'assets/markdowns/my-experience-with-stable-diffusion-and-how-it-transformed-my-images.md',
+    date: new Date('Jan 20 2023'),
+  },
+  {
+    slug: 'old-it-dood-learning-has-no-limits',
+    title: 'Old IT Dood (Learning has no limits)',
+    subTitle: 'Jun 14 2022',
+    summary: `I met an older software developer named Gopalakrishnan who had been learning new technologies from my videos. He shared with me his background as a successful developer in the 90s but struggled to adapt to new technologies. I was impressed by his determination to improve his skills and shared my knowledge on modern software development practices and SaaS platforms. It was a reminder that it's never too late to learn and grow in the tech industry. I will continue to create and share knowledge through my videos to help others like Gopalakrishnan.`,
+    imageUrl: 'assets/images/old-it-dood-learning-has-no-limits.jpeg',
+    bannerImageUrl: 'assets/images/1200x300.png',
+    markDownFile: 'assets/markdowns/old-it-dood-learning-has-no-limits.md',
+    date: new Date('Jun 14 2022'),
+  },
+  {
+    slug: 'two-days-workshop-in-payilagam',
+    title: 'Two Days Workshop in Payilagam',
+    subTitle: 'Nov 23 2023',
+    summary: `I have recently led a successful 2-day seminar on HTML and CSS at Paiylagam Institute, where the students were attentive and interactive. The instructor effectively imparted their knowledge of web development to the students, who actively participated in hands-on exercises and a real-world project. At the end of each day, the instructor held a fun review game to reinforce the material learned, creating a balanced and enjoyable learning experience. The seminar was a resounding success and the instructor looks forward to leading more seminars in the future.`,
+    imageUrl: 'assets/images/pailagam.jpeg',
+    bannerImageUrl: 'assets/images/1200x300.png',
+    markDownFile: 'assets/markdowns/two-days-workshop-in-payilagam.md',
+    date: new Date('Jun 17 2022'),
+  },
+];
